8323d0cc59a3862cf93d13a42abfd97df230ffd6
Hauke Mehrtens <hauke@hauke-m.de>
2018-09-02 21:46:58 +0200

    Add firmware_request_cache()
    An empty implementation should be ok, then we do not tell the kernel that this firmware file shgould be cachec and it will take a little bit longer on rsumt. This should be ok.

--- backport/backport-include/linux/firmware.h
+++ backport/backport-include/linux/firmware.h
@@ -10,4 +10,11 @@
 #define firmware_request_nowarn(fw, name, device) request_firmware(fw, name, device)
 #endif
 
+#if LINUX_VERSION_IS_LESS(4,17,0)
+static inline int firmware_request_cache(struct device *device, const char *name)
+{
+	return 0;
+}
+#endif
+
 #endif /* __BACKPORT_LINUX_FIRMWARE_H */
