hauke/backports

commit 30a378636e473278ff65800b1fe58822e36d716c

backports: speed up netdev destructor spatch by two orders of magnitude
As reported by Hauke, and analyzed by Julia, the netdev destructor
spatch is really slow because <... ...> means 0 or more hits, and
thus the spatch has to be applied on all files. However, we don't
really need it to be this way, so use <+... ...+> which allows it
to reject almost all files immediately, cutting down the processing
time by more than two orders of magnitude as it only has to touch a
handful of files, rather than all.

Signed-off-by: Johannes Berg <johannes.berg@intel.com>

patches/0079-netdev-destructor.cocci
@@ -5,11 +5,11 @@ identifier TRUE =~ "true";
 @@
 C(...)
 {
-	<...
+	<+...
 -	NDEV->needs_free_netdev = TRUE;
 -	NDEV->priv_destructor = D;
 +	netdev_set_priv_destructor(NDEV, D);
-	...>
+	...+>
 }
 
 @r2 depends on r1@

commit a791c5e4c4a6ebbff0777f6752fafa1cfc50e947

backports: Make netdev destructor spatch apply correctly again
The netdev destructor spatch did not apply to the wil6210 driver any
more, because there the calls were done in a different order compared to
the spatch. Add an additional rule which applies when the order is
changed.

Fixes: 30a378636e47 ("backports: speed up netdev destructor spatch by two orders of magnitude")
Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

patches/0079-netdev-destructor.cocci
@@ -67,6 +67,38 @@ if (<+... RET ...+>) {
 	...>
 }
 
+@r11@
+struct net_device *NDEV;
+identifier D, C;
+identifier TRUE =~ "true";
+@@
+C(...)
+{
+	<+...
+-	NDEV->priv_destructor = D;
+-	NDEV->needs_free_netdev = TRUE;
++	netdev_set_priv_destructor(NDEV, D);
+	...+>
+}
+
+@r12 depends on r11@
+identifier r11.D, r11.C;
+fresh identifier E = "__" ## D;
+@@
+
++#if LINUX_VERSION_IS_LESS(4,12,0)
++static void E(struct net_device *ndev)
++{
++	D(ndev);
++	free_netdev(ndev);
++}
++#endif
++
+C(...)
+{
+	...
+}
+
 @r5@
 struct net_device *NDEV;
 identifier TRUE =~ "true";